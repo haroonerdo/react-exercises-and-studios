d9f0570f1e6f7dc7d1a512c528a268cc80cebf89
Ayse Erdogan <aerdogan@AErdogan-Fyler.attlocal.net>
2024-03-13 03:04:16 -0500

    React Part-1 Exercises March-11

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import MovieList from './components/MovieList.jsx';
 import ChoresList from './components/ChoresList.jsx';
 import BookList from './components/BookList.jsx';
+import HobbyLinks from './components/HobbyLinks.jsx';
 
 function App() {
   return (
